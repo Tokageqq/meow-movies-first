e2b4d3f418efc91c79ba14f3836ac2c4837a7697
Tokageqq <rasul2004sosma@gmail.com>
2022-11-11 10:55:39 +0600

    first commit

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+    baseUrl: 'https://api.themoviedb.org/3/',
+    apiKey: '7b783fe938b3fb14a820b1445c8a3ed4',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default apiConfig;
